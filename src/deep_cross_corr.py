f6e7311ccad688c23d77c4be00ccef282d47efaa
DCalhas <davidcalhas@hotmail.com>
2019-09-24 16:57:39 +0100

    try debugging

--- src/deep_cross_corr.py
+++ src/deep_cross_corr.py
@@ -23,6 +23,8 @@ from keras.optimizers import Adam
 from keras.losses import mae
 
 import tensorflow.compat.v1 as tf
+tf.enable_eager_execution()
+
 
 import keras.backend as K
 
@@ -275,12 +277,12 @@ if __name__ == "__main__":
 	print(X_train_eeg.shape)
 
 	#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-	#cfg = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True, gpu_options=gpu_options)
-	#cfg.gpu_options.allow_growth = True
-	#session = tf.Session(config=cfg)
-	#with session:
-	#session.run(tf.global_variables_initializer())
-	history = multi_modal_model.fit([X_train_eeg, X_train_bold], 
-		tr_y, epochs=n_epochs, 
-		batch_size=n_partitions*100)#, validation_data=([X_test_eeg, X_test_bold], te_y))
+	cfg = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True, gpu_options=gpu_options)
+	cfg.gpu_options.allow_growth = True
+	session = tf.Session(config=cfg)
+	with session:
+		session.run(tf.global_variables_initializer())
+		history = multi_modal_model.fit([X_train_eeg, X_train_bold], 
+			tr_y, epochs=n_epochs, 
+			batch_size=n_partitions*100)#, validation_data=([X_test_eeg, X_test_bold], te_y))
 
