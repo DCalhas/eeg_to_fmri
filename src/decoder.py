d7875ee6bad9d6e3926e59b8ecbaa87a7a11901f
DCalhas <davidcalhas@hotmail.com>
2019-09-25 10:34:02 +0100

    decoder work

--- src/decoder.py
+++ src/decoder.py
@@ -40,25 +40,16 @@ regularizer = regularizers.l1(reg_l)
 
 if __name__ == "__main__":
 
-	#mask = fmri_utils.get_population_mask()
+	mask = fmri_utils.get_population_mask()
 
 	#reading data and spliting data into train and test by individuals
-	#X_train, y_train = deep_cross_corr.get_data(list(range(14)), masker=mask)
-	#X_test, y_test = deep_cross_corr.get_data(list(range(14, 16)), masker=mask)
+	eeg_train, bold_train = deep_cross_corr.get_data(list(range(14)), masker=mask)
+	eeg_test, bold_test = deep_cross_corr.get_data(list(range(14, 16)), masker=mask)
 
-	#X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], X_train.shape[3], 1)
-	#X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], X_test.shape[3], 1)
+	eeg_train = eeg_train.reshape(eeg_train.shape[0], eeg_train.shape[1], eeg_train.shape[2], eeg_train.shape[3], 1)
+	eeg_test = eeg_test.reshape(eeg_test.shape[0], eeg_test.shape[1], eeg_test.shape[2], eeg_test.shape[3], 1)
 
-	#eeg_input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3], 1)
-
-	#kernel_size = (X_train.shape[1], X_train.shape[2], 1)
-
-	#eeg network
-	#eeg_network = deep_cross_corr.eeg_network(eeg_input_shape, kernel_size, output_dim=output_dim,
-	#	activation_function=activation_function, regularizer=regularizer)
-	#print(eeg_network.summary())
-
-	#LOAD MODEL WEIGHTS
+	#LOAD MODEL WEIGHTS - EEG NETWORK
 	json_file = open('multi_model/eeg_demo_0.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
@@ -68,6 +59,4 @@ if __name__ == "__main__":
 
 	print(eeg_network.summary())
 
-	history = eeg_network.fit([X_train_eeg, X_train_bold], 
-		tr_y, epochs=n_epochs, 
-		batch_size=n_partitions*100)#, validation_data=([X_test_eeg, X_test_bold], te_y))
+	print(eeg_network.predict(eeg_train))
