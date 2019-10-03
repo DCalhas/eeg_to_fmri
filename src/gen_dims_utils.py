f766679170a1ec024dcad0722db5c4e71bfbd3e3
DCalhas <davidcalhas@hotmail.com>
2019-10-03 23:06:50 +0100

    layer kernel and stride wise done, todo search algorithm #5

---
+++ src/gen_dims_utils.py
@@ -0,0 +1,52 @@
+from tensorflow.python.keras.utils import conv_utils
+
+import numpy as np
+
+def get_possible_kernel_size_conv(input_shape, output_shape):
+	#list of tuples where #1 is kernel size and #2 is stride size
+	possible_combinations = []
+
+	for stride in range(1, input_shape):
+		for kernel in range(1, input_shape):
+
+			new_dim = conv_utils.conv_output_length(input_shape, 
+				kernel, 
+				padding='valid', 
+				stride=stride)
+
+			#only accept dim = output desired and that are bigger than half the 
+			if(new_dim == output_shape and kernel > stride):
+				possible_combinations += [(kernel, stride)]
+
+	return possible_combinations
+
+def get_possible_kernel_size_deconv(input_shape, output_shape):
+	#list of tuples where #1 is kernel size and #2 is stride size
+	possible_combinations = []
+
+	for stride in range(1, output_shape):
+		for kernel in range(1, output_shape):
+
+			new_dim = conv_utils.deconv_output_length(input_shape, 
+				kernel, 
+				padding='valid', 
+				output_padding=None, 
+				stride=stride, 
+				dilation=1)
+
+			#only accept dim = output desired and that are bigger than half the 
+			if(new_dim == output_shape and kernel > stride):
+				possible_combinations += [(kernel, stride)]
+
+	return possible_combinations
+
+def add_generated_dim(generated, input_shape, output_shape, func):
+	possible = func(input_shape, output_shape)
+
+	pos = list(range(len(possible)))
+	generated_kernel_stride = possible[np.random.choice(pos)]
+	
+	generated['kernel'] += (generated_kernel_stride[0],)
+	generated['stride'] += (generated_kernel_stride[1],)
+
+	return generated
--- src/iterative_naive_nas.py
+++ src/iterative_naive_nas.py
@@ -1,50 +1,12 @@
-import tensorflow.compat.v1 as tf
-
 import numpy as np
 
+import tensorflow.compat.v1 as tf
 from tensorflow.python.keras.utils import conv_utils
 
+import gen_dims_utils
 
-layers = [tf.keras.layers.Dense, tf.keras.layers.Conv2D, tf.keras.layers.Conv2DTranspose]
-
-
-def get_possible_kernel_size_conv(input_shape, output_shape):
-	#list of tuples where #1 is kernel size and #2 is stride size
-	possible_combinations = []
-
-	for stride in range(1, input_shape):
-		for kernel in range(1, input_shape):
-
-			new_dim = conv_utils.conv_output_length(input_shape, 
-				kernel, 
-				padding='valid', 
-				stride=stride)
-
-			#only accept dim = output desired and that are bigger than half the 
-			if(new_dim == output_shape and kernel > stride):
-				possible_combinations += [(kernel, stride)]
-
-	return possible_combinations
-
-def get_possible_kernel_size_deconv(input_shape, output_shape):
-	#list of tuples where #1 is kernel size and #2 is stride size
-	possible_combinations = []
-
-	for stride in range(1, input_shape):
-		for kernel in range(1, input_shape):
-
-			new_dim = conv_utils.deconv_output_length(input_shape, 
-				kernel, 
-				padding='valid', 
-				output_padding=None, 
-				stride=stride, 
-				dilation=0)
-
-			#only accept dim = output desired and that are bigger than half the 
-			if(new_dim == output_shape and kernel > stride):
-				possible_combinations += [(kernel, stride)]
-
-	return possible_combinations
+layers = [tf.keras.layers.Dense, tf.keras.layers.Conv2D, tf.keras.layers.Conv2DTranspose, 
+								tf.keras.layers.Conv3D, tf.keras.layers.Conv3DTranspose]
 
 
 class iterative_naive_nas:
@@ -54,14 +16,15 @@ class iterative_naive_nas:
 		self.iterations = 0
 
 
-	#Dense layer is simple to generate
-	def generate_layer_Dense(self, input_shape, output_shape):
-		if(type(output_shape) is not int):
-			return None
-		return layers[0](output_shape, input_shape=input_shape)
+	######################################################################################################################################
+	#
+	#												Generation of Kernel and Stride Sizes
+	#
+	######################################################################################################################################
 
-	def generate_layer_Conv1D(self, input_shape, output_shape):
-		possible = get_possible_kernel_size_conv(input_shape[0], output_shape[0])
+
+	def generate_kernel_stride_Conv1D(self, input_shape, output_shape):
+		possible = gen_dims_utils.get_possible_kernel_size_conv(input_shape[0], output_shape[0])
 
 		pos = list(range(len(possible)))
 		generated_kernel_stride = possible[np.random.choice(pos)]
@@ -69,40 +32,94 @@ class iterative_naive_nas:
 		return {'kernel': (generated_kernel_stride[0],),
 				'stride': (generated_kernel_stride[1],)}
 
-	def generate_layer_Conv1DTranspose(self, input_shape, output_shape):
-		return None
 
-	def generate_layer_Conv2D(self, input_shape, output_shape):
-		generated = self.generate_layer_Conv1D((input_shape[0],1), (output_shape[0], 1))
-
-		possible = get_possible_kernel_size_conv(input_shape[1], output_shape[1])
+	def generate_kernel_stride_Conv1DTranspose(self, input_shape, output_shape):
+		possible = gen_dims_utils.get_possible_kernel_size_deconv(input_shape[0], output_shape[0])
 
 		pos = list(range(len(possible)))
 		generated_kernel_stride = possible[np.random.choice(pos)]
 
-		generated['kernel'] += (generated_kernel_stride[0],)
-		generated['stride'] += (generated_kernel_stride[1],)
+		return {'kernel': (generated_kernel_stride[0],),
+				'stride': (generated_kernel_stride[1],)}
 
-		return generated
 
-	def generate_layer_Conv2DTranspose(self, input_shape, output_shape):
-		return None
+	def generate_kernel_stride_Conv2D(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv1D((input_shape[0],1), (output_shape[0], 1))
 
-	def generate_layer_Conv3D(self, input_shape, output_shape):
-		generated = self.generate_layer_Conv2D((input_shape[0], input_shape[1],1), (output_shape[0], output_shape[1], 1))
+		return gen_dims_utils.add_generated_dim(generated, input_shape[1], output_shape[1], gen_dims_utils.get_possible_kernel_size_conv)
 
-		possible = get_possible_kernel_size_conv(input_shape[2], output_shape[2])
 
-		pos = list(range(len(possible)))
-		generated_kernel_stride = possible[np.random.choice(pos)]
+	def generate_kernel_stride_Conv2DTranspose(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv1DTranspose((input_shape[0],1), (output_shape[0], 1))
 
-		generated['kernel'] += (generated_kernel_stride[0],)
-		generated['stride'] += (generated_kernel_stride[1],)
+		return gen_dims_utils.add_generated_dim(generated, input_shape[1], output_shape[1], gen_dims_utils.get_possible_kernel_size_deconv)
 
-		return generated
 
-	def generate_layer_Conv3DTranspose(self, input_shape, output_shape):
-		return None
+	def generate_kernel_stride_Conv3D(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv2D((input_shape[0], input_shape[1],1), (output_shape[0], output_shape[1], 1))
+
+		return gen_dims_utils.add_generated_dim(generated, input_shape[2], output_shape[2], gen_dims_utils.get_possible_kernel_size_conv)
+
+
+	def generate_kernel_stride_Conv3DTranspose(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv2DTranspose((input_shape[0], input_shape[1], 1), (output_shape[0], output_shape[1], 1))
+		
+		return gen_dims_utils.add_generated_dim(generated, input_shape[2], output_shape[2], gen_dims_utils.get_possible_kernel_size_deconv)
+
+
+	######################################################################################################################################
+	#
+	#															Build Layer Functions
+	#
+	######################################################################################################################################
+
+	#Dense layer is simple to generate
+	def build_layer_Dense(self, input_shape, output_shape):
+		if(type(output_shape) is not int):
+			return None
+		return layers[0](output_shape, input_shape=input_shape)
+
+
+	def build_layer_Conv2D(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv2D(input_shape, output_shape)
+
+		return layers[1](1, kernel_size=generated['kernel'], strides=generated['stride'], input_shape=input_shape)
+
+
+	def build_layer_Conv2DTranspose(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv2DTranspose(input_shape, output_shape)
+
+		return layers[2](1, kernel_size=generated['kernel'],
+							strides=generated['stride'], 
+							padding='valid', 
+							output_padding=None,
+							dilation_rate=(1, 1),
+							input_shape=input_shape)
+
+
+	def build_layer_Conv3D(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv3D(input_shape, output_shape)
+
+		return layers[3](1, kernel_size=generated['kernel'], strides=generated['stride'], input_shape=input_shape)
+
+
+	def build_layer_Conv3DTranspose(self, input_shape, output_shape):
+		generated = self.generate_kernel_stride_Conv3DTranspose(input_shape, output_shape)
+
+		return layers[4](1, kernel_size=generated['kernel'],
+							strides=generated['stride'], 
+							padding='valid', 
+							output_padding=None,
+							dilation_rate=(1, 1, 1),
+							input_shape=input_shape)
+
+
+	######################################################################################################################################
+	#
+	#													Iterative Neural Architecture Search
+	#
+	######################################################################################################################################
+
 
 	def search(self):
 		return []
@@ -111,4 +128,9 @@ class iterative_naive_nas:
 if __name__ == "__main__":
 	nas = iterative_naive_nas()
 
-	print(nas.generate_layer_Conv3D((10, 10, 10, 1), (2, 2, 3, 1)))
+	#print(nas.generate_layer_Conv3DTranspose((10, 10, 10, 1), (30, 30, 30, 1)))
+	#print(nas.generate_kernel_stride_Conv2D((10, 10, 1), (5, 5, 1)))
+
+	model = tf.keras.Sequential()
+	model.add(nas.build_layer_Conv3DTranspose((5, 5, 5, 1), (10, 10, 10, 1)))
+	print(model.summary())
