d01335d7e7f0ca659f83ac125d1257dd0d8bceff
DCalhas <davidcalhas@hotmail.com>
2019-11-19 22:52:29 +0000

    minor changes

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -37,11 +37,13 @@ def load_data(train_instances, test_instances, n_voxels=None, roi=None, roi_ica_
 									n_voxels=n_voxels, roi=roi, 
 									roi_ica_components=roi_ica_components)
 
-	eeg_train = eeg_train.reshape(eeg_train.shape[0], eeg_train.shape[1], eeg_train.shape[2], eeg_train.shape[3], 1)
-	eeg_test = eeg_test.reshape(eeg_test.shape[0], eeg_test.shape[1], eeg_test.shape[2], eeg_test.shape[3], 1)
+	if(train_instances):
+		eeg_train = eeg_train.reshape(eeg_train.shape[0], eeg_train.shape[1], eeg_train.shape[2], eeg_train.shape[3], 1)
+		eeg_test = eeg_test.reshape(eeg_test.shape[0], eeg_test.shape[1], eeg_test.shape[2], eeg_test.shape[3], 1)
 
-	bold_train = bold_train.reshape(bold_train.shape[0], bold_train.shape[1], bold_train.shape[2], 1)
-	bold_test = bold_test.reshape(bold_test.shape[0], bold_test.shape[1], bold_test.shape[2], 1)
+	if(test_instances):
+		bold_train = bold_train.reshape(bold_train.shape[0], bold_train.shape[1], bold_train.shape[2], 1)
+		bold_test = bold_test.reshape(bold_test.shape[0], bold_test.shape[1], bold_test.shape[2], 1)
 
 	return eeg_train, bold_train, eeg_test, bold_test
 
