all training methods fixed for decoder issue, now testing: cosine average of voxels

src/bayesian_optimization.py
@@ -168,7 +168,7 @@ def hidden_layer_NAS_BO(multi_modal_instance, eeg_domain, bold_domain, decoder_d
 			epochs=40, 
 			encoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
 			decoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
-			loss_function=losses_utils.get_reconstruction_cosine_loss,
+			loss_function=losses_utils.get_reconstruction_cosine_voxel_loss,
 			batch_size=current_batch_size, linear_combination=current_loss_coefficient,
 			X_val_eeg=X_val_eeg,
 			X_val_bold=X_val_bold,
@@ -184,11 +184,16 @@ def hidden_layer_NAS_BO(multi_modal_instance, eeg_domain, bold_domain, decoder_d
 		#	tv_y=tv_y)
 		#	eeg_train=eeg_train, bold_train=bold_train, eeg_val=eeg_val, bold_val=bold_val, bold_network=bold_network)
 		#validation_loss = custom_training.ranked_synthesis_training(X_train_eeg, X_train_bold, tr_y, eeg_network, decoder_network, multi_modal_model, 
-		#	epochs=40, optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+		#	epochs=40, 
+		#	encoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+		#	decoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
 		#	batch_size=current_batch_size,
 		#	X_val_eeg=X_val_eeg,
 		#	X_val_bold=X_val_bold,
-		#	tv_y=tv_y)
+		#	tv_y=tv_y,
+		#	eeg_train=eeg_train, bold_train=bold_train, eeg_val=eeg_val, bold_val=bold_val, bold_network=bold_network,
+		#	X_bold_train_target=X_bold_train_target,
+		#	X_bold_val_target=X_bold_val_target)
 		#validation_loss = custom_training.dcca_training(X_train_eeg, X_train_bold, tr_y, eeg_network, decoder_network, multi_modal_model, 
 		#	epochs=40, optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
 		#	batch_size=current_batch_size, linear_combination=current_loss_coefficient, dcca_output=current_dcca_output,
@@ -362,23 +367,32 @@ def NAS_BO(multi_modal_instance, output_shape_domain):
 
 		#this try should be checked
 		#validation_loss = custom_training.ranked_synthesis_training(X_train_eeg, X_train_bold, tr_y, eeg_network, decoder_network, multi_modal_model, 
-		#	epochs=40, optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+		#	epochs=40, 
+		#	encoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+		#	decoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
 		#	batch_size=current_batch_size,
 		#	X_val_eeg=X_val_eeg,
 		#	X_val_bold=X_val_bold,
 		#	tv_y=tv_y,
-		#	eeg_train=eeg_train, bold_train=bold_train, eeg_val=eeg_val, bold_val=bold_val, bold_network=bold_network)
+		#	eeg_train=eeg_train, bold_train=bold_train, eeg_val=eeg_val, bold_val=bold_val, bold_network=bold_network,
+		#	X_bold_train_target=X_bold_train_target,
+		#	X_bold_val_target=X_bold_val_target)
 		#validation_loss = custom_training.adversarial_training(X_train_eeg, X_train_bold, tr_y, eeg_network, decoder_network, multi_modal_model, 
-		#	epochs=40, optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+		#	epochs=40, 
+		#	discriminator_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
+    	#	generator_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate), 
 		#	batch_size=current_batch_size, linear_combination=current_loss_coefficient,
 		#	X_val_eeg=X_val_eeg,
 		#	X_val_bold=X_val_bold,
-		#	tv_y=tv_y)
+		#	tv_y=tv_y,
+		#	eeg_train=eeg_train, bold_train=bold_train, eeg_val=eeg_val, bold_val=bold_val,
+		#	X_bold_train_target=X_bold_train_target,
+    	#	X_bold_val_target=X_bold_val_target)
 		validation_loss = custom_training.linear_combination_training(X_train_eeg, X_train_bold, tr_y, eeg_network, decoder_network, multi_modal_model, 
 			epochs=40, 
 			encoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
 			decoder_optimizer=tf.keras.optimizers.Adam(learning_rate=current_learning_rate),
-			loss_function=losses_utils.get_reconstruction_cosine_loss,
+			loss_function=losses_utils.get_reconstruction_cosine_voxel_loss,
 			batch_size=current_batch_size, linear_combination=current_loss_coefficient,
 			X_val_eeg=X_val_eeg,
 			X_val_bold=X_val_bold,

src/custom_training.py
@@ -353,7 +353,8 @@ ranked_synthesis_training
 
 trains a encoder and decoder
 it gives the encoder a contrastive loss, learn most correlated instances
-it gives the decoder a reconstruction losses
+it gives the decode  File "/home/david/eeg_informed_fmri/src/custom_training.py", line 378, in ranked_synthesis_training
+r a reconstruction losses
 """
 def ranked_synthesis_training(X_train_eeg, X_train_bold, tr_y, eeg_network, 
     decoder_model, multi_modal_model, epochs=10, 
@@ -373,11 +374,6 @@ def ranked_synthesis_training(X_train_eeg, X_train_bold, tr_y, eeg_network,
         validation = True
 
     global_step = tf.Variable(0)
-    encoder_loss, _ = grad_multi_encoder(multi_modal_model, 
-                             [X_train_eeg, X_train_bold], 
-                             tr_y, 0, linear_combination)
-
-    print("Encoder Loss: ", tf.keras.backend.eval(encoder_loss))
 
     #train encoder first
     for epoch in range(epochs):
@@ -673,7 +669,7 @@ def adversarial_training(X_train_eeg, X_train_bold, tr_y, eeg_network,
             sys.stdout.flush()
 
     shared_eeg_val = eeg_network(tf.gather_nd(eeg_val, X_val_eeg))
-    return tf.keras.backend.eval(loss_decoder(decoder_model(shared_eeg_val), tf.gather_nd(bold_val, tf.gather_nd(bold_val, X_val_bold))))
+    return tf.keras.backend.eval(loss_decoder(decoder_model(shared_eeg_val), tf.gather_nd(bold_val, X_val_bold)))
 
 
 