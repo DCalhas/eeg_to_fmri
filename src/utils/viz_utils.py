b40fa6c22eadf759b3c0e25e706badc83b7953e1
DCalhas <davidcalhas@hotmail.com>
2020-02-20 14:51:48 +0000

    fixed

--- src/utils/viz_utils.py
+++ src/utils/viz_utils.py
@@ -200,10 +200,10 @@ def _plot_voxels(real_set, synth_set, individual=0, voxels=None, y_bottom=None, 
         for voxel in range(n_voxels):
         	real_voxel = real_set[voxel]
         	synth_voxel = synth_set[voxel]
-        	
-        	if(normalized):
-            	real_voxel = real_voxel/norm(real_voxel)
-            	synth_voxel = synth_voxel/norm(synth_voxel)
+
+			if(normalized):
+				real_voxel = real_voxel/norm(real_voxel)
+				synth_voxel = synth_voxel/norm(synth_voxel)
 
 
             _plot_voxel(real_voxel, synth_voxel, 
