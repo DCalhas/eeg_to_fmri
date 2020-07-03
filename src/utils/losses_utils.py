b37316788715007e06c6f8190898d7c17c379602
DCalhas <davidcalhas@hotmail.com>
2020-07-03 11:03:40 +0100

    margin contrastive

--- src/utils/losses_utils.py
+++ src/utils/losses_utils.py
@@ -12,9 +12,9 @@ import tensorflow.keras.backend as K
 #
 ######################################################################################################################
 
-def contrastive_loss(y_true, y_pred):
+def contrastive_loss(y_true, y_pred, margin=1.0):
 	square_pred = K.square(y_pred)
-	margin_square = K.square(K.maximum(1.0 - y_pred, 0))
+	margin_square = K.square(K.maximum(margin - y_pred, 0))
 	return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
 
 
