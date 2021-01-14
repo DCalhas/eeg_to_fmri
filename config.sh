f11fdf01e2b43ee27a4a39379ada4c23d661b2c1
DCalhas <davidcalhas@hotmail.com>
2021-01-14 11:39:16 +0000

    configuration to run code

---
+++ config.sh
@@ -0,0 +1,25 @@
+
+source $HOME/anaconda3/bin/activate
+conda create -n eeg_fmri python=3.7
+source $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate eeg_fmri
+conda install tensorflow-gpu==2.1.0
+pip install tensorflow-estimator==2.1.*
+pip install -r requirements.txt
+
+echo "I: Setting up datasets directory"
+
+
+FILE=datasets/zipped_datasets/01.zip
+if [ -f "$FILE" ]; then
+    echo "I: Datasets already downloaded."
+else 
+    echo "I: Please download datasets indicated in the Datasets Description of the paper"
+    echo "I: Organize them in datasets/01 and datasets/02"
+    echo "I: If the zipped datasets file is available please unzip them in the directories specified"
+    printf "I: If not please download them from:\nFirst dataset:\nEEG1.zip: https://osf.io/mx8ze/download \nEEG2.zip: https://osf.io/2zmup/download\nfMRI.zip: https://osf.io/vd5yz/download\nSecond dataset: http://openfmri.s3.amazonaws.com/tarballs/ds000116_R2.0.0_raw.tgz\n"
+    mkdir datasets
+    mkdir datasets/01
+    mkdir datasets/02
+    mkdir datasets/zipped_datasets
+fi
