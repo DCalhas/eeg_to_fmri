66798f69465b1970407b057de037d8e5ac63f909
DCalhas <davidcalhas@hotmail.com>
2021-01-21 16:48:03 +0000

    tuple

--- src/utils/search_algorithms.py
+++ src/utils/search_algorithms.py
@@ -24,8 +24,8 @@ class Bayesian_Optimization:
 		self.X_val = X_val
 		
 	def optimize(self, hyperparameters):
-		hyperparameters = self.input_shape + tuple(hyperparameters[0])
-		print(hyperparameters)
+		hyperparameters = (self.input_shape,) + tuple(hyperparameters[0])
+		
 		model = self.model_class.build(*hyperparameters)
 		optimizer = self.optimizer(learning_rate)
 		loss_fn = tf.keras.losses.MAE
