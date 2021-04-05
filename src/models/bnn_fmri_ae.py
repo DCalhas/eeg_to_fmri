9c2370568883d6eeb478053fec18a0e26fbf7b85
DCalhas <daivdcalhas@hotmail.com>
2021-04-05 16:09:08 +0100

    import LocallyConnected3DFlipout

--- src/models/bnn_fmri_ae.py
+++ src/models/bnn_fmri_ae.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-from layers.locally_connected import LocallyConnected3D
+from layers.locally_connected import LocallyConnected3D, LocallyConnected3DFlipout
 
 
 
