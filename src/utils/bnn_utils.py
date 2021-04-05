7d73e66a1eb30ab9b4f1719daf21e790a7c5c6bd
DCalhas <daivdcalhas@hotmail.com>
2021-04-05 12:24:16 +0100

    new loss to compare

--- src/utils/bnn_utils.py
+++ src/utils/bnn_utils.py
@@ -29,6 +29,11 @@ def combined_abs_loss(y_true, y_pred):
 	
 	return tf.reduce_mean(((1/variance+1e-9)*(y_pred[0] - y_true)**2)/2 + variance/2, axis=(1,2,3))
 
+def combined_abs_diff_log_loss(y_true, y_pred):
+	variance = tf.math.abs(y_pred[1])
+	
+	return tf.reduce_mean((-variance*(y_pred[0] - y_true)**2)/2 + (tf.math.log(variance))/2, axis=(1,2,3))
+
 def combined_abs_diff_loss(y_true, y_pred):
 	variance = tf.math.abs(y_pred[1])
 	
