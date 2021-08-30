9a7db75d280c44548bdb40a9ec0a5a5391b40189
DCalhas <daivdcalhas@hotmail.com>
2021-08-30 15:01:49 +0100

    fixing error

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -224,8 +224,8 @@ def create_eeg_bold_pairs(eeg, bold, interval_eeg=2, n_volumes=300, n_individual
 
     print(eeg.shape)
     print(bold.shape)
-    print(individual*(n_volumes)+interval_eeg, individual*(n_volumes)+n_volumes-interval_eeg)
     for individual in range(n_individuals):
+        print(individual*(n_volumes)+interval_eeg, individual*(n_volumes)+n_volumes-interval_eeg)
         for index_volume in range(individual*(n_volumes)+interval_eeg, individual*(n_volumes)+n_volumes-interval_eeg):
             
             x_eeg[index_volume] = np.transpose(eeg[index_volume-interval_eeg:index_volume], (1,2,0))
