bb49373a525639ed64e439fc0ec9e074c9bf7199
DCalhas <daivdcalhas@hotmail.com>
2021-10-04 11:09:56 +0100

    debugging gradients

--- src/utils/train.py
+++ src/utils/train.py
@@ -11,7 +11,7 @@ def apply_gradient(model, optimizer, loss_fn, x, y):
         loss = loss_fn(y, model(x))
     gradients = tape.gradient(loss, model.trainable_variables)
     print(gradients)
-    return
+    return gradients,model.trainable_variables
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return tf.reduce_mean(loss)
 
@@ -113,6 +113,7 @@ def train(train_set, model, opt, loss_fn, epochs=10, val_set=None, u_architectur
         n_batches = 0
         
         for batch_set in train_set.repeat(1):
+            return train_step(model, batch_set, opt, loss_fn, u_architecture=u_architecture)
             batch_loss = train_step(model, batch_set, opt, loss_fn, u_architecture=u_architecture).numpy()
             loss += batch_loss
             n_batches += 1
