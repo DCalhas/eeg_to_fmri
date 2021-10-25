c4c318c73cfd0c80f955cb253a710d5296ce8108
DCalhas <daivdcalhas@hotmail.com>
2021-10-25 08:50:22 +0100

    test other architecture

--- src/utils/process_utils.py
+++ src/utils/process_utils.py
@@ -147,13 +147,13 @@ def batch_prediction(shared_flattened_predictions, batch_path, batch, epoch, net
 	outfilter=int(theta[12])
 	local=True
 	#alter this
-	with open(na_path + "/na_specification_"+str(network+1), "rb") as f:
-		na_specification = pickle.load(f)
-	#na_specification = ([(10,20,2),(10,20,2)], 
-	#					[(1,1,1),(1,1,1)],
-	#					True,
-	#					(2,2,1),
-	#					(1,1,1))
+	#with open(na_path + "/na_specification_"+str(network+1), "rb") as f:
+	#	na_specification = pickle.load(f)
+	na_specification = ([(10,20,2),(10,20,2)], 
+						[(1,1,1),(1,1,1)],
+						True,
+						(2,2,1),
+						(1,1,1))
 
 	#load or build model
 	with tf.device('/CPU:0'):
