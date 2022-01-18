2cab4198504bb7b3db92d9c9ede536cf171453b4
DCalhas <daivdcalhas@hotmail.com>
2022-01-18 12:08:50 +0000

    fixing lrp propagation with network defined by a different graph

--- src/utils/lrp.py
+++ src/utils/lrp.py
@@ -134,6 +134,7 @@ class LRP_EEG(tf.keras.layers.Layer):
 					print(layer-layer_bias)
 					print(layer)
 					print(len(activations))
+					print(model.layer[layer].name)
 					R = lrp(activations[layer-layer_bias-1], R, model.layers[layer])
 				else:
 					R = lrp(X, R, model.layers[layer])
