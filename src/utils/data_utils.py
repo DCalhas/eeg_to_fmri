0820743fde0dc0dff399923e2416084ac7977871
DCalhas <daivdcalhas@hotmail.com>
2022-02-07 14:27:11 +0000

    loading 10

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -350,6 +350,9 @@ def create_clf_pairs(n_individuals, data, labels, recording_time=90, interval_ee
     i = 0
     for ind in range(n_individuals):
         for time in range(0, recording_time, interval_eeg):
+            print(time)
+            print((ind*recording_time)+time,(ind*recording_time)+(time+interval_eeg))
+            print()
             X[i] = np.transpose(data[(ind*recording_time)+time:(ind*recording_time)+(time+interval_eeg)], (1,2,0))
             y[i] = labels[ind]
             i+=1
