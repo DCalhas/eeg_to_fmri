c170812c12a1def2dbf3fe9832d31996a6679cac
DCalhas <daivdcalhas@hotmail.com>
2022-02-07 14:00:36 +0000

    loading 10

--- src/utils/preprocess_data.py
+++ src/utils/preprocess_data.py
@@ -104,8 +104,8 @@ def dataset(dataset, n_individuals=8, interval_eeg=6, ind_volume_fit=True, raw_e
 def dataset_clf(dataset, n_individuals=8, mutate_bands=False, f_resample=2, raw_eeg=False, raw_eeg_resample=False, eeg_limit=False, eeg_f_limit=134, file_output=None, standardize_eeg=False, interval_eeg=10, verbose=False):
 
 	if(dataset=="10"):
-		n_individuals_train = 20
-		n_individuals_test = 7
+		n_individuals_train = 30
+		n_individuals_test = 13
 		recording_time=203
 		eeg_limit=True 
 		eeg_f_limit=135
