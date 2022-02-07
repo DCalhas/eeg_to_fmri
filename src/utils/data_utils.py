8dd0c8495bf0905f18f8bfcbe2fa58d23af8f789
DCalhas <daivdcalhas@hotmail.com>
2022-02-07 14:40:17 +0000

    loading 10

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -352,11 +352,11 @@ def create_clf_pairs(n_individuals, data, labels, recording_time=90, interval_ee
         for time in range(0, recording_time, interval_eeg):
             print(time)
             print((ind*recording_time)+time,(ind*recording_time)+(time+interval_eeg))
-            print((ind*recording_time)+(time+interval_eeg) >= ind+1*recording_time)
-
-            X[i] = np.transpose(data[(ind*recording_time)+time:(ind*recording_time)+(time+interval_eeg)], (1,2,0))
-            y[i] = labels[ind]
-            i+=1
+            if((ind*recording_time)+(time+interval_eeg) < (ind+1)*recording_time):
+                X[i] = np.transpose(data[(ind*recording_time)+time:(ind*recording_time)+(time+interval_eeg)], (1,2,0))
+                y[i] = labels[ind]
+                i+=1
+                print(i, (n_individuals*recording_time)//interval_eeg)
     
     return X, y
 #############################################################################################################
