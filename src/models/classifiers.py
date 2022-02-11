6dcdf9736559a3e27ccac78dc853d451043aa832
DCalhas <daivdcalhas@hotmail.com>
2022-02-11 15:41:08 +0000

    classifeir with view integrated

--- src/models/classifiers.py
+++ src/models/classifiers.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-
+from models.eeg_to_fmri import pretrained_EEG_to_fMRI
 
 class LinearClassifier(tf.keras.Model):
 	"""
@@ -16,3 +16,30 @@ class LinearClassifier(tf.keras.Model):
 	def call(self, X):
 		
 		return self.linear(self.flatten(X))
+
+
+"""
+classifier of synthesized EEG view
+
+"""
+
+class view_EEG_classifier(tf.keras.Model):
+
+    """
+    Inputs:
+        - EEG_to_fMRI: model
+        - tupel: input_shape, eeg input shape
+    """
+    def __init__(self, model, input_shape, seed=None):
+        super(view_EEG_classifier, self).__init__()
+        
+        self.view = pretrained_EEG_to_fMRI(model, input_shape, seed=seed)
+        self.clf = classifiers.LinearClassifier()
+    
+    def build(self, input_shape):
+        self.view.build(input_shape)
+        self.clf.build(self.view.decoder.output_shape)
+    
+    def call(self, X):
+        return self.clf(self.view(X))
+        
