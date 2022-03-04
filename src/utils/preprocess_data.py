de03d08d07d3f421fed1032386679257763e1b33
DCalhas <daivdcalhas@hotmail.com>
2022-03-04 16:06:22 +0000

    checking snaity

--- src/utils/preprocess_data.py
+++ src/utils/preprocess_data.py
@@ -233,8 +233,6 @@ class Dataset_CLF_CV:
 		assert fold < self.n_individuals, "A fold is within {0, 1, ..., N-1} for a total of N folds"
 		
 		step=(self.n_individuals//self.folds)
-		print((0,fold*self.ind_time*step), (fold*self.ind_time*step+self.ind_time*step, ":"))
-		print(fold*self.ind_time*step,fold*self.ind_time*step+self.ind_time*step)
 		return (np.append(self.X[:fold*self.ind_time*step], self.X[fold*self.ind_time*step+self.ind_time*step:], axis=0), 
 						np.append(self.y[:fold*self.ind_time*step], self.y[fold*self.ind_time*step+self.ind_time*step:], axis=0)), \
 				(self.X[fold*self.ind_time*step:fold*self.ind_time*step+self.ind_time*step], 
