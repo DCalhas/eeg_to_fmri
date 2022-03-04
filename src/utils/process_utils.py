d0624522b42e9c6b11256d118f9b16382a30f273
DCalhas <daivdcalhas@hotmail.com>
2022-03-04 16:07:58 +0000

    checking snaity

--- src/utils/process_utils.py
+++ src/utils/process_utils.py
@@ -579,7 +579,7 @@ def cv_opt(reg_constants, fold_loocv, n_folds_cv, view, dataset, learning_rate, 
 					linearCLF = classifiers.LinearClassifier(regularizer=tf.keras.regularizers.l1_l2(l1=l1_reg, l2=l2_reg))
 				linearCLF.build(X_train.shape)
 
-			train.train(train_set, linearCLF, optimizer, loss_fn, epochs=epochs, val_set=None, u_architecture=False, verbose=False, verbose_batch=False)
+			train.train(train_set, linearCLF, optimizer, loss_fn, epochs=50, val_set=None, u_architecture=False, verbose=False, verbose_batch=False)
 			#evaluate
 			print(y_test)
 			print(linearCLF(X_test))
