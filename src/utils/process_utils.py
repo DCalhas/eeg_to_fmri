68edf9375d87b1e0e456ee568834f68762560ef9
DCalhas <daivdcalhas@hotmail.com>
2022-03-04 15:46:28 +0000

    trying one output

--- src/utils/process_utils.py
+++ src/utils/process_utils.py
@@ -583,7 +583,7 @@ def cv_opt(reg_constants, fold_loocv, n_folds_cv, view, dataset, learning_rate, 
 
 			train.train(train_set, linearCLF, optimizer, loss_fn, epochs=epochs, val_set=None, u_architecture=False, verbose=False, verbose_batch=False)
 			#evaluate
-			score+=loss_fn(y_test[:,:,0], linearCLF(X_test))
+			score+=loss_fn(y_test[:,0], linearCLF(X_test))
 			
 		value[0] = score
 
