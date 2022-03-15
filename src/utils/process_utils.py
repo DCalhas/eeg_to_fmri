e0fda1a1233906088843350f2a704c706cc6cd93
DCalhas <daivdcalhas@hotmail.com>
2022-03-15 11:48:38 +0000

    regularization does not need to be so high

--- src/utils/process_utils.py
+++ src/utils/process_utils.py
@@ -588,8 +588,8 @@ def cv_opt(fold_loocv, n_folds_cv, view, dataset, epochs, gpu_mem, seed, path_la
 		if(np.isnan(value[0])):
 			value[0] = 1/1e-9
 
-	hyperparameters = [{'name': 'l1', 'type': 'continuous','domain': (1e-10, 10.)}, 
-						{'name': 'l2', 'type': 'continuous', 'domain': (1e-10, 10.)},
+	hyperparameters = [{'name': 'l1', 'type': 'continuous','domain': (1e-10, 1.)}, 
+						{'name': 'l2', 'type': 'continuous', 'domain': (1e-10, 1.)},
 						{'name': 'batch_size', 'type': 'discrete', 'domain': (1,2,4,8,16,32,64)},
 						{'name': 'learning_rate', 'type': 'continuous', 'domain': (1e-10, 1e-4)}]
 	optimizer = GPyOpt.methods.BayesianOptimization(f=optimize_wrapper, 
