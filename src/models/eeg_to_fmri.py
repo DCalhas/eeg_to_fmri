4358f057584eef7fb6e6106eb04058f5576bdda7
DCalhas <daivdcalhas@hotmail.com>
2022-03-15 15:43:23 +0000

    trying feature selection smoothing

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -444,10 +444,11 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
         #split flow in two
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[16]).__name__)(
                     pretrained_model.layers[4].layers[16].units,
-                    activation=activation,
+                    activation=None,
                     kernel_regularizer=regularizer,
                     bias_regularizer=regularizer,
                     trainable=True)(x)
+        z = tf.keras.layers.ReLU(max_value=1.0)(z)
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(
                     pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
 
