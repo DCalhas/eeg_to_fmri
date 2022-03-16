f6281c4b58eae35102ca8bbdc1af8568e9cdcd62
DCalhas <daivdcalhas@hotmail.com>
2022-03-16 13:33:23 +0000

    dft works for representation but not for classification

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -448,13 +448,13 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
                     kernel_regularizer=regularizer,
                     bias_regularizer=regularizer,
                     trainable=True)(x)
+        z = tf.keras.layers.LayerNormalization()(z)
         z = tf.keras.layers.ReLU(max_value=1.0)(z)
         
         #try smoothing feature selection
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
         z = DCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
         shape_smoothing=(5,5,2)
-        z = tf.keras.layers.LayerNormalization()(z)
         z = z*tf.keras.layers.ZeroPadding3D(padding=((0, z.shape[1]-shape_smoothing[0]), (0, z.shape[2]-shape_smoothing[1]), (0, z.shape[3]-shape_smoothing[2])))(tf.ones((1,)+shape_smoothing+(1,)))[:,:,:,:,0]
         z = iDCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
 
