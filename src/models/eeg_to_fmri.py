bf0f170f485ec093c62f6c4e27607ce91d4da170
DCalhas <daivdcalhas@hotmail.com>
2022-03-16 15:42:25 +0000

    droput after layer

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -449,9 +449,10 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
                     kernel_regularizer=regularizer,
                     bias_regularizer=regularizer,
                     trainable=True)(x)
+        z = tf.keras.layers.Dropout(0.5)(z)
         z = tf.keras.layers.LayerNormalization()(z)
         z = tf.keras.layers.ReLU(max_value=1.0)(z)
-        z = tf.keras.layers.Dropout(0.5)(z)
+        
         
         #try smoothing feature selection
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
