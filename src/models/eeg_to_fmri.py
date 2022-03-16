4f45b7fad9ff191780114850906a2fae49288303
DCalhas <daivdcalhas@hotmail.com>
2022-03-16 15:04:48 +0000

    trying after dct

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -448,7 +448,6 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
                     kernel_regularizer=regularizer,
                     bias_regularizer=regularizer,
                     trainable=True)(x)
-        z = tf.keras.layers.LayerNormalization()(z)        
         
         #try smoothing feature selection
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
@@ -459,6 +458,7 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
 
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(
                     pretrained_model.layers[4].layers[17].target_shape)(z)
+        z = tf.keras.layers.LayerNormalization()(z)
         z = tf.keras.layers.ReLU(max_value=1.0)(z)
 
         #upsampling
