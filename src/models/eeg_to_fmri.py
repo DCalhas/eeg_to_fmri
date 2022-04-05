83e3c3f68516382875d8ad388b7e8f4d8ff3de89
DCalhas <daivdcalhas@hotmail.com>
2022-04-05 12:35:29 +0100

    trying internal feature selection reduction

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -507,11 +507,10 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
     def call(self, x1):
         #l0 norm??? counts of 
         z = self.q_decoder(x1).numpy()
-        z_mask = self.decoder(x1)
+        z_mask = 1.-self.decoder(x1)
 
         #be similar
-        self.add_loss(tf.keras.losses.KLDivergence(z*z_mask, z))
+        self.add_loss(tf.keras.losses.MeanAbsoluteError(z*z_mask, z))
         #l0 norm - close to because of ReLU activation
-        self.add_loss(tf.reduce_sum(z_mask))
-
+        self.add_loss(-tf.reduce_sum(z_mask))#it should select all and omit only regions that are important
         return [z*z_mask, z_mask]
