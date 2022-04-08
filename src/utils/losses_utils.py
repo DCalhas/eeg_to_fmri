fixing with layer normalization, along with 1.-z_mask, softmax in test check and Adam optimizer

src/utils/losses_utils.py
@@ -453,9 +453,9 @@ def entropy_mae_loss(y_true, y_pred):
     sigma_c = y_pred[3]
     sigma_r = y_pred[4]
 
-    regression = tf.reduce_mean(tf.abs(y_pred[1]-y_pred[2]), axis=(1,2,3))
-    classification = tf.keras.losses.MeanSquaredError()(y_true, y_pred[0])#tf.keras.losses.CategoricalCrossentropy(from_logits=True)(y_true, y_pred[0]) 
-    return (1/sigma_c)*classification + (1/sigma_r)*regression + tf.math.log(sigma_c*sigma_r)
+    regression = tf.keras.losses.MeanSquaredError()(y_pred[1],y_pred[2])
+    classification = tf.keras.losses.MeanSquaredError()(y_true, y_pred[0])
+    return (1/sigma_r)*regression + (1/sigma_c)*classification + tf.math.log(sigma_c*sigma_r)
     
 
 """

src/utils/process_utils.py
@@ -590,7 +590,7 @@ def cv_opt(fold_loocv, n_folds_cv, view, dataset, epochs, gpu_mem, seed, path_la
 			#evaluate
 			linearCLF.training=False
 			#evaluate according to final AUC in validation sets
-			y_pred=np.append(y_pred, linearCLF(X_test).numpy()[:,1])
+			y_pred=np.append(y_pred, tf.keras.activations.softmax(linearCLF(X_test)).numpy()[:,1])
 			y_true=np.append(y_true, y_test[:,1])
 			if(view=="fmri"):
 				ssim=np.append(ssim, metrics.ssim(tf.data.Dataset.from_tensor_slices((X_test, linearCLF.view.q_decoder(X_test))).batch(1), linearCLF.view, two_inputs=False))