f2b24ff17b9d0156fc8ac44b83349b2ac810088e
DCalhas <daivdcalhas@hotmail.com>
2022-04-12 13:46:45 +0100

    no batch normalikzation

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -466,7 +466,6 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
         z = iDCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
         z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(
                     pretrained_model.layers[4].layers[17].target_shape)(z)
-        z = tf.keras.layers.BatchNormalization(axis=-1)(z)
 
         #upsampling
         x = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[16]).__name__)(
