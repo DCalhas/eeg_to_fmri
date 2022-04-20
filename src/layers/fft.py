8ad0d13a07211f3624e9b25f8c72492c1d8fa373
DCalhas <daivdcalhas@hotmail.com>
2022-04-20 14:15:44 +0100

    debugging error

--- src/layers/fft.py
+++ src/layers/fft.py
@@ -359,7 +359,9 @@ class variational_iDCT3D(tf.keras.layers.Layer):
 		if(self.dependent):
 			x_cond=tf.matmul(a=tf.reshape(x, (tf.shape(x)[0], tf.shape(x)[1]*tf.shape(x)[2]*tf.shape(x)[3],)), b=self.w)
 			print(tf.math.multiply(rand_coefs1, x_cond))
-			rand_coefs1 = tf.math.multiply(x_cond, rand_coefs1)
+			shape_coef1 = tf.shape(rand_coefs1)
+			rand_coefs1 = tf.math.multiply(x_cond, tf.reshape(rand_coefs1, (shape_coef1[0]*shape_coef1[1]*shape_coef1[2])))
+			print(tf.shape(rand_coefs1))
 			rand_coefs2 = tf.math.multiply(x_cond, rand_coefs2)
 			rand_coefs3 = tf.math.multiply(x_cond, rand_coefs3)
 		z = tf.pad(x, rand_paddings1, constant_values=1.0)*tf.pad(rand_coefs1, in_paddings1, constant_values=1.0)
