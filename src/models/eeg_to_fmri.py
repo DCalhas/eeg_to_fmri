af9b7f96b4373f8daf074233d9572c82dac15d8a
DCalhas <daivdcalhas@hotmail.com>
2022-05-11 11:01:01 +0100

    trying with not perturbance on low resolutions

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -261,7 +261,7 @@ class EEG_to_fMRI(tf.keras.Model):
             if(variational_iDFT):
                 assert type(variational_coefs) is tuple
                 x = variational_iDCT3D(*(latent_shape + self.fmri_ae.in_shape[:3] + variational_coefs), 
-                                        coefs_perturb=True, dependent=variational_iDFT_dependent, posterior_dimension=variational_iDFT_dependent_dim, distribution=variational_dist)(x)
+                                        coefs_perturb=False, dependent=variational_iDFT_dependent, posterior_dimension=variational_iDFT_dependent_dim, distribution=variational_dist)(x)
             else:
                 x = padded_iDCT3D(latent_shape[0], latent_shape[1], latent_shape[2],
                             out1=self.fmri_ae.in_shape[0], out2=self.fmri_ae.in_shape[1], out3=self.fmri_ae.in_shape[2])(x)
