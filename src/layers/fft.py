202774361ddb9d17888b2a650b51fe6f8840ce9e
DCalhas <daivdcalhas@hotmail.com>
2022-05-26 14:00:11 +0100

    remove print

--- src/layers/fft.py
+++ src/layers/fft.py
@@ -452,7 +452,6 @@ class variational_iDCT3D(tf.keras.layers.Layer):
 			rand_coefs3 = tf.matmul(x_cond3, rand_coefs3)#shape = [None, F] = [Batch, F]
 
 		regularize_coefs = tf.concat([rand_coefs1, rand_coefs2, rand_coefs3], 1)
-		print(regularize_coefs.shape)
 
 		rand_coefs1 = tf.reshape(rand_coefs1, (tf.shape(rand_coefs1)[0],)+self.shape_normal1)
 		rand_coefs2 = tf.reshape(rand_coefs2, (tf.shape(rand_coefs2)[0],)+self.shape_normal2)
