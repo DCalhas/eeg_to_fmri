f163ebb0910994cd17fcb43b3b8b2ea4b3dc10bd
DCalhas <daivdcalhas@hotmail.com>
2022-06-15 14:12:36 +0100

    padded

--- src/benchmarks/run_liu.sh
+++ src/benchmarks/run_liu.sh
@@ -1,5 +1,5 @@
-python liu.py 01 -variational -variationalDCT -interval_eeg 1 -seed 2
-python liu.py 01 -variational -variationalDCT -interval_eeg 1 -seed 3
-python liu.py 01 -variational -variationalDCT -interval_eeg 1 -seed 5
-python liu.py 01 -variational -variationalDCT -interval_eeg 1 -seed 7
-python liu.py 01 -variational -variationalDCT -interval_eeg 1 -seed 11
+python liu.py 01 -padded -interval_eeg 1 -seed 2
+python liu.py 01 -padded -interval_eeg 1 -seed 3
+python liu.py 01 -padded -interval_eeg 1 -seed 5
+python liu.py 01 -padded -interval_eeg 1 -seed 7
+python liu.py 01 -padded -interval_eeg 1 -seed 11
