c44e4702d8e55e928e353109b31605c456c2aa4a
DCalhas <daivdcalhas@hotmail.com>
2022-06-21 11:20:19 +0100

    no epochs to see saving of results

--- src/benchmarks/run_liu.sh
+++ src/benchmarks/run_liu.sh
@@ -1,5 +1,1 @@
 python liu.py uncertainty 01 -variationalDCT -variational -interval_eeg 1 -save -seed 2
-python liu.py uncertainty 01 -variationalDCT -variational -interval_eeg 1 -save -seed 3
-python liu.py uncertainty 01 -variationalDCT -variational -interval_eeg 1 -save -seed 5
-python liu.py uncertainty 01 -variationalDCT -variational -interval_eeg 1 -save -seed 7
-python liu.py uncertainty 01 -variationalDCT -variational -interval_eeg 1 -save -seed 11
