929467e5bd9963f8d2f1bac508eb7836ad3f3db6
DCalhas <daivdcalhas@hotmail.com>
2022-07-01 11:30:03 +0100

    run all for liu

--- src/benchmarks/run_liu.sh
+++ src/benchmarks/run_liu.sh
@@ -13,3 +13,8 @@ python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 3
 python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 5
 python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 7
 python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 11
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 2
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 3
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 5
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 7
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 11
