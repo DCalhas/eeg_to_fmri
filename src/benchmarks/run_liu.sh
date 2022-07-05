d1976e1b87601f31526205662fb636feade6ba4c
DCalhas <daivdcalhas@hotmail.com>
2022-07-05 09:04:00 +0100

    more memory

--- src/benchmarks/run_liu.sh
+++ src/benchmarks/run_liu.sh
@@ -1,20 +1,20 @@
-python liu.py metrics $1 -interval_eeg 1 -save -seed 2
-python liu.py metrics $1 -interval_eeg 1 -save -seed 3
-python liu.py metrics $1 -interval_eeg 1 -save -seed 5
-python liu.py metrics $1 -interval_eeg 1 -save -seed 7
-python liu.py metrics $1 -interval_eeg 1 -save -seed 11
-python liu.py metrics $1 -padded -interval_eeg 1 -save -seed 2
-python liu.py metrics $1 -padded -interval_eeg 1 -save -seed 3
-python liu.py metrics $1 -padded -interval_eeg 1 -save -seed 5
-python liu.py metrics $1 -padded -interval_eeg 1 -save -seed 7
-python liu.py metrics $1 -padded -interval_eeg 1 -save -seed 11
-python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 2
-python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 3
-python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 5
-python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 7
-python liu.py metrics $1 -variational -interval_eeg 1 -save -seed 11
-python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 2
-python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 3
-python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 5
-python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 7
-python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -seed 11
+python liu.py metrics $1 -interval_eeg 1 -save -memory_limit 6000 -seed 2
+python liu.py metrics $1 -interval_eeg 1 -save -memory_limit 6000 -seed 3
+python liu.py metrics $1 -interval_eeg 1 -save -memory_limit 6000 -seed 5
+python liu.py metrics $1 -interval_eeg 1 -save -memory_limit 6000 -seed 7
+python liu.py metrics $1 -interval_eeg 1 -save -memory_limit 6000 -seed 11
+python liu.py metrics $1 -padded -interval_eeg 1 -save -memory_limit 6000 -seed 2
+python liu.py metrics $1 -padded -interval_eeg 1 -save -memory_limit 6000 -seed 3
+python liu.py metrics $1 -padded -interval_eeg 1 -save -memory_limit 6000 -seed 5
+python liu.py metrics $1 -padded -interval_eeg 1 -save -memory_limit 6000 -seed 7
+python liu.py metrics $1 -padded -interval_eeg 1 -save -memory_limit 6000 -seed 11
+python liu.py metrics $1 -variational -interval_eeg 1 -save -memory_limit 6000 -seed 2
+python liu.py metrics $1 -variational -interval_eeg 1 -save -memory_limit 6000 -seed 3
+python liu.py metrics $1 -variational -interval_eeg 1 -save -memory_limit 6000 -seed 5
+python liu.py metrics $1 -variational -interval_eeg 1 -save -memory_limit 6000 -seed 7
+python liu.py metrics $1 -variational -interval_eeg 1 -save -memory_limit 6000 -seed 11
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 2
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 3
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 5
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 7
+python liu.py metrics $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 11
