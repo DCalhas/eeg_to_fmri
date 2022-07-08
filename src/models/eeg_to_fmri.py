3ad67f37cc2116fedca3794aa2d7e55bd153e718
DCalhas <daivdcalhas@hotmail.com>
2022-07-08 09:38:51 +0100

    trying to load new layer with padded layers

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -362,7 +362,7 @@ class EEG_to_fMRI(tf.keras.Model):
     def from_config(cls, config):
         return cls(**config)
 
-        
+
 
 custom_objects={"Topographical_Attention": Topographical_Attention,
                 "EEG_to_fMRI": EEG_to_fMRI,
@@ -486,14 +486,16 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
         z = tf.keras.layers.ReLU()(z)
 
         print(pretrained_model.layers[4].summary())
+        print(pretrained_model.layers[4].layers[17])
+        print(pretrained_model.layers[4].layers[17].__dict__())
         
         #try smoothing feature selection
-        z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
-        z = DCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
-        shape_smoothing=(5,5,3)
-        z = z*tf.keras.layers.ZeroPadding3D(padding=((0, z.shape[1]-shape_smoothing[0]), (0, z.shape[2]-shape_smoothing[1]), (0, z.shape[3]-shape_smoothing[2])))(tf.ones((1,)+shape_smoothing+(1,)))[:,:,:,:,0]
-        z = iDCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
-        z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(
+        #z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
+        #z = DCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
+        #shape_smoothing=(5,5,3)
+        #z = z*tf.keras.layers.ZeroPadding3D(padding=((0, z.shape[1]-shape_smoothing[0]), (0, z.shape[2]-shape_smoothing[1]), (0, z.shape[3]-shape_smoothing[2])))(tf.ones((1,)+shape_smoothing+(1,)))[:,:,:,:,0]
+        #z = iDCT3D(*pretrained_model.layers[4].layers[17].target_shape[:-1])(z)
+        #z = getattr(tf.keras.layers, type(pretrained_model.layers[4].layers[17]).__name__)(
                     pretrained_model.layers[4].layers[17].target_shape)(z)
         
         #perform brain segmentation with mask
