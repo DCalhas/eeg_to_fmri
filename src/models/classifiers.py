cae0fbe018ce4a2b6e161e0d0e13b62870986fef
DCalhas <daivdcalhas@hotmail.com>
2022-08-23 11:08:39 +0100

    trying to implement contrastive loss

--- src/models/classifiers.py
+++ src/models/classifiers.py
@@ -99,10 +99,6 @@ class ContrastiveClassifier(tf.keras.Model):
 
     def build(self, input_shape):
         self.view.build(input_shape)
-        if(self.view.aleatoric):
-            self.clf.build(self.view.q_decoder.output_shape[:-1]+(2,))#additional dimension for aleatoric uncertainty
-        else:
-            self.clf.build(self.view.q_decoder.output_shape)
 
 
     def call(self, X):
