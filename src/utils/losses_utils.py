7ea25106596c8ab2b14b8ba39f948571c740e24d
DCalhas <daivdcalhas@hotmail.com>
2022-08-30 13:50:30 +0100

    layer style is not being well defined

--- src/utils/losses_utils.py
+++ src/utils/losses_utils.py
@@ -44,6 +44,7 @@ class ContrastiveLoss(tf.keras.losses.Loss):
         if(tf.rank(distance)==2):
             similar=tf.expand_dims(similar, axis=-1)
 
+        print(similar*distance + (1-similar)*(tf.abs(distance-self.m)))
         return similar*distance + (1-similar)*(tf.abs(distance-self.m))
 
 
