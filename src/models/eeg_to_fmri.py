806762ae4d0fe93cf8ba2b29136c150709bfa906
DCalhas <daivdcalhas@hotmail.com>
2022-08-30 09:34:40 +0100

    Sinusoids projection separated from the sinusoids

--- src/models/eeg_to_fmri.py
+++ src/models/eeg_to_fmri.py
@@ -501,6 +501,7 @@ class pretrained_EEG_to_fMRI(tf.keras.Model):
     
     def build_decoder(self, pretrained_model, input_shape, output_encoder, activation=None, attention_scores=None, regularizer=None, feature_selection=False, segmentation_mask=False, seed=None):
         
+        x=output_encoder
         
         if("Fourier" in type(pretrained_model.layers[4].layers[11]).__name__):
             index=11
