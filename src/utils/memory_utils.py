282dea0f1a960786a626b3a73084bb0dcfa3ce63
DCalhas <daivdcalhas@hotmail.com>
2022-10-10 16:29:26 +0100

    limiting memory python

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -3,4 +3,3 @@ import resource
 def limit_CPU_memory(bytes):
 	soft, hard = resource.getrlimit(resource.RLIMIT_DATA)
 	resource.setrlimit(resource.RLIMIT_DATA, (bytes, bytes))
-	resource.setrlimit(resource.RLIMIT_AS, (bytes, bytes))
