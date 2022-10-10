101f92ad1d2ca6e9c9fe3df3e6ac665b65a6a660
DCalhas <daivdcalhas@hotmail.com>
2022-10-10 15:53:19 +0100

    get arguments of datasets from oficial files

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -2,6 +2,5 @@ import resource
 
 def limit_CPU_memory(bytes):
 	soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-	print(hard)
-	print(soft)
-	resource.setrlimit(resource.RLIMIT_AS, (bytes, hard))
+	
+	resource.setrlimit(resource.RLIMIT_AS, (soft, bytes))
