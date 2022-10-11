2a3de7bdb7e97739ee664a167b590507f282834d
DCalhas <daivdcalhas@hotmail.com>
2022-10-11 08:47:41 +0100

    debugging

--- src/utils/train.py
+++ src/utils/train.py
@@ -125,7 +125,9 @@ def train(train_set, model, opt, loss_fn, epochs=10, val_set=None, u_architectur
         
         for batch_set in train_set.repeat(1):
 
+            print("TRAIN STEP")
             batch_loss = train_step(model, batch_set, opt, loss_fn, u_architecture=u_architecture).numpy()
+            print("FINISHED TRAIN STEP")
             loss += batch_loss
             n_batches += 1
             gc.collect()
