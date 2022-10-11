526de91880dba7745ea810c3c2d077523c40c0dc
DCalhas <daivdcalhas@hotmail.com>
2022-10-11 08:42:10 +0100

    trying to check why it is not training#

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -2,4 +2,4 @@ import resource
 
 def limit_CPU_memory(bytes):
 	soft, hard = resource.getrlimit(resource.RLIMIT_DATA)
-	resource.setrlimit(resource.RLIMIT_DATA, (bytes, bytes))
+	resource.setrlimit(resource.RLIMIT_DATA, (bytes, hard))
