6b795af5b81773e3a1799f6baabf7e1b4142561f
DCalhas <daivdcalhas@hotmail.com>
2022-10-12 12:27:27 +0100

    limiting more memory

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -4,5 +4,5 @@ def limit_CPU_memory(bytes):
 	soft, hard = resource.getrlimit(resource.RLIMIT_DATA)
 	resource.setrlimit(resource.RLIMIT_DATA, (bytes, hard))#heap of the process
 	resource.setrlimit(resource.RLIMIT_AS, (1024*1024*1024*26, hard))#virtual memory allocation
-	resource.setrlimit(resource.RLIMIT_RSS, (1024*1024*1024*9, hard))#resident memory
+	resource.setrlimit(resource.RLIMIT_RSS, (1024*1024*1024*8, hard))#resident memory
 	#resource.setrlimit(resource.RLIMIT_SWAP, (1024*1024*1024*2, hard))
