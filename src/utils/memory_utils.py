94d9e78c79e3207c250b8e332fbf9f729571151d
DCalhas <daivdcalhas@hotmail.com>
2022-10-12 13:55:33 +0100

    limit on the number of processesg

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -5,4 +5,5 @@ def limit_CPU_memory(bytes):
 	resource.setrlimit(resource.RLIMIT_DATA, (bytes, hard))#heap of the process
 	resource.setrlimit(resource.RLIMIT_AS, (1024*1024*1024*26, hard))#virtual memory allocation
 	resource.setrlimit(resource.RLIMIT_RSS, (1024*1024*1024*8, 1024*1024*1024*10))#resident memory
+	resource.setrlimit(resource.RLIMIT_NPROC, (4,4))
 	#resource.setrlimit(resource.RLIMIT_SWAP, (1024*1024*1024*2, hard))
