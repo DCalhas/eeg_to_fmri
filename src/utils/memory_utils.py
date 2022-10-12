aa32fe521277abbe1b14f9155fd5a917832af7bd
DCalhas <daivdcalhas@hotmail.com>
2022-10-12 12:11:58 +0100

    trying to set resident memory

--- src/utils/memory_utils.py
+++ src/utils/memory_utils.py
@@ -3,4 +3,6 @@ import resource
 def limit_CPU_memory(bytes):
 	soft, hard = resource.getrlimit(resource.RLIMIT_DATA)
 	resource.setrlimit(resource.RLIMIT_DATA, (bytes, hard))
-	resource.setrlimit(resource.RLIMIT_AS, (1024*1024*1024*23, hard))
+	resource.setrlimit(resource.RLIMIT_AS, (1024*1024*1024*26, hard))
+	resource.setrlimit(resource.RLIMIT_RSS, (1024*1024*1024*16, hard))
+	resource.setrlimit(resource.RLIMIT_SWAP, (1024*1024*1024*2, hard))
