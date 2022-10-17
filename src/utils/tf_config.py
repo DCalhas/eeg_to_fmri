9e180fc9f77e44d7c31b8d01eabe104d56f7379d
DCalhas <daivdcalhas@hotmail.com>
2022-10-17 16:07:46 +0100

    synchronous execution and determinism

--- src/utils/tf_config.py
+++ src/utils/tf_config.py
@@ -25,6 +25,7 @@ def setup_tensorflow(memory_limit, device="CPU", run_eagerly=False, set_primary_
 
 	#set number of threads
 	if(set_tf_threads):
+		tf.config.experimental.set_synchronous_execution(True)
 		tf.config.threading.set_inter_op_parallelism_threads(MAX_N_THREADS)
 		tf.config.threading.set_intra_op_parallelism_threads(MAX_N_THREADS)
 
@@ -35,4 +36,5 @@ def set_seed(seed=42):
 	random.seed(seed)
 	np.random.seed(seed)
 	tf.random.set_seed(seed)
+	tf.config.experimental.enable_op_determinism()
 	os.environ['TF_DETERMINISTIC_OPS'] = '1'
