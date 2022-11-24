2306beb614a950f0995ca611d66c7e99f9f73f56
DCalhas <daivdcalhas@hotmail.com>
2022-11-24 09:03:54 +0000

    main done

--- biclusters/src/main.py
+++ biclusters/src/main.py
@@ -41,7 +41,13 @@ parser.add_argument('-min_lift', default=1.2, type=float, help="Minimum lift to 
 parser.add_argument('-nr_labels', default=5, type=int, help="Number of bins to discretize the data")
 parser.add_argument('-min_columns', default=3, type=int, help="Minimum columns to stop searching for more")
 parser.add_argument('-best_K', default=10, type=int, help="Top biclusters to save")
+parser.add_argument('-metrics_path', default=os.environ['EEG_FMRI']+"/biclusters/clusters", type=str, help="Metrics save path.")
 
+
+"""
+Example run:
+$ python main.py ground_truth 10 -background_cutoff_low -resolution 20,20,10 -threshold_mask -min_biclusters 100 -min_lift 1.3 -nr_labels 5 -min_columns 10
+"""
 
 opt = parser.parse_args()
 
@@ -59,6 +65,7 @@ min_columns=opt.min_columns
 best_K=opt.best_K
 threshold_mask=opt.threshold_mask
 threshold_mask_value=opt.threshold_mask_value
+metrics_path=opt.metrics_path
 
 bicpams_parameters=bicpy.DEFAULT_PARAMS
 bicpams_parameters['min_biclusters']=min_biclusters
@@ -73,13 +80,18 @@ if(background_cutoff):
 	setting+="_original_masked"
 elif(background_cutoff):
 	setting+="_low_masked_"+str(resolution[0])+"x"+str(resolution[1])+"x"+str(resolution[2])
+setting+="_lift_"+min_lift+"_clusters_"+min_biclusters+"_labels_"+nr_labels+"_columns_"+min_columns
+
+#create dir setting if not exists
+if(not os.path.exists(metrics_path+"/"+ setting)):
+	os.makedirs(metrics_path+"/"+ setting)
 
 #set seed and tensorflow GPU memory
 tf_config.set_seed(seed=42)#02 20
 tf_config.setup_tensorflow(device="GPU", memory_limit=1500)
 
 #define labels path and view
-path_labels="../../metrics/10_synth_01_style_prior_bayesian/"
+path_labels=os.environ['EEG_FMRI']+"/metrics/10_synth_01_style_prior_bayesian/"
 view="fmri"
 
 #load data
@@ -96,19 +108,20 @@ with open("./view_"+mode+"."+format_file, "w") as f:
 		f.write(to_bicpams.build_arff(views_downsampled, y_true, y_pred, resolution, format_file, cutoff_low=background_cutoff_low, brain_mask=brain_mask)[1])
 	f.close()
 
+#run BicPAMS
 bicpy.run(bicpams_parameters, "./view_"+mode+"."+format_file)
 
-
-#TODO the other notebook
-
+#process BicPAMS results
 bics_patterns=patterns.Bics_Patterns.build("./output/result.txt", X_view, relu=None, low=True, resolution=resolution)
 
 for k in range(best_K):
-
 	best_index=bics_patterns.get_best(delete=True)
 	print("I:", str(k+1), "bicluster at index", str(best_index),"with lifts:", bics_patterns.get(best_index).lifts)
 	masked_data=bics_patterns.apply_mask(best_index, brain_mask=brain_mask)
 
+	indices=np.unique(np.where(masked_data>=threshold_mask_value)[0])
 	if(threshold_mask):
 		masked_data[np.where(masked_data<threshold_mask_value)]=0.1
 		masked_data[np.where(masked_data>=threshold_mask_value)]=0.99
+
+	fig = viz_utils.plot_3D_representation_projected_slices_alpha(np.mean(X_view[indices], axis=0),res_img=np.mean(X_view[indices],axis=0),alpha_img=np.amax(masked_data[indices], axis=0),slice_label=False,uncertainty=True,legend_colorbar=r"$H$",save=True, save_path=path_labels+"bic_"+str(k+1)+".pdf", max_min_legend=["Inhibitory","Excitatory"])
