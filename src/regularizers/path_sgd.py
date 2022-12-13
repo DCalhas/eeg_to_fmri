fc997017454d88ec0fe583fc05ae612bc0a63b49
DCalhas <daivdcalhas@hotmail.com>
2022-12-13 13:26:59 +0000

    path until the end

--- src/regularizers/path_sgd.py
+++ src/regularizers/path_sgd.py
@@ -141,8 +141,8 @@ class PathOptimizer(OPTIMIZER):
 		#in the special case of ViewLatentContrastiveClassifier we have to do this, so we do not have two flowsS
 		if(type(path_model).__name__ in ["ViewLatentContrastiveClassifier"]):#, "ViewLatentLikelihoodClassifier"]):
 			path_model.training=False
-			path_model=path_model.view.eeg_encoder
-			self.n_params=len(path_model.trainable_variables)
+			#path_model=path_model.view.eeg_encoder
+			#self.n_params=len(path_model.trainable_variables)
 
 		#compute scale
 		with tf.GradientTape() as tape:
