3bc362e00d4855fa335d90f59a92acd858733ea6
DCalhas <daivdcalhas@hotmail.com>
2022-12-13 10:45:15 +0000

    ensure convergence of latent space

--- src/utils/losses_utils.py
+++ src/utils/losses_utils.py
@@ -58,6 +58,7 @@ class ContrastiveLoss(tf.keras.losses.Loss):
         if(tf.rank(distance)==2):
             similar=tf.expand_dims(similar, axis=-1)
 
+        return similar*distance + (1-similar)*(tf.abs(distance-self.m))
         return similar*(-1.*distance) + (1-similar)*(1.*distance)
 
 
