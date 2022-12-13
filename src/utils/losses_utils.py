8b23792668a14db779ddfdc13ccd81f22f37dca1
DCalhas <daivdcalhas@hotmail.com>
2022-12-13 12:29:08 +0000

    each one gets a loss

--- src/utils/losses_utils.py
+++ src/utils/losses_utils.py
@@ -47,7 +47,7 @@ class SeparationEntropyLoss(tf.keras.losses.Loss):
         self.nll=tf.keras.losses.BinaryCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)
 
     def call(self, y_true, y_pred):
-        return self.nll(y_true, tf.reduce_mean(y_pred[0], axis=1))+0.*self.nll(y_true, y_pred[1])
+        return self.nll(tf.repeat(tf.expand_dims(y_true, axis=-1), repeats=y_pred[0].shape[-1], axis=-1), y_pred[0])+0.*self.nll(y_true, y_pred[1])
 
 
 class ContrastiveLoss(tf.keras.losses.Loss):
