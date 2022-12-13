74990bcf6ccc4464f54abb8e288d3f43056a662d
DCalhas <daivdcalhas@hotmail.com>
2022-12-13 10:46:45 +0000

    distance

--- src/models/classifiers.py
+++ src/models/classifiers.py
@@ -169,6 +169,7 @@ class ViewLatentContrastiveClassifier(tf.keras.Model):
             s1=s1/tf.norm(s1.numpy(), ord=2)
             s2=s2/tf.norm(s2.numpy(), ord=2)
 
+            return [(z1[0],z2[0]), tf.abs(s1-s2), self.clf(z1[0].numpy(), training=self.training), self.clf(z2[0].numpy(), training=self.training)]
             return [(z1[0],z2[0]), self.dot([s1,s2]), self.clf(z1[0].numpy(), training=self.training), self.clf(z2[0].numpy(), training=self.training)]
 
         #also when training only for classification
