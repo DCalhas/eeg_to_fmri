b73772b9fa6a3e6770725c28c7a6a46ad85d6b00
DCalhas <daivdcalhas@hotmail.com>
2022-12-14 14:30:17 +0000

    no layer normalization

--- src/layers/fourier_features.py
+++ src/layers/fourier_features.py
@@ -107,10 +107,10 @@ class RandomFourierFeatures(tf.keras.layers.Layer):
 		self.normalization=normalization
 		
 		if(normalization=="layer"):
-			self.layer_normalization=tf.keras.layers.LayerNormalization(beta_initializer=tf.constant_initializer(np.pi/2), gamma_initializer=tf.constant_initializer(np.pi/2), trainable=False)
+			#self.layer_normalization=tf.keras.layers.LayerNormalization(beta_initializer=tf.constant_initializer(np.pi/2), gamma_initializer=tf.constant_initializer(np.pi/2), trainable=False)
 			self.reg_normalization=MaxNormalization(mu=np.pi/2, l=0.5*(2/np.pi)**0.5, p=2)
 		elif(normalization=="tanh"):
-			self.layer_normalization=TanhNormalization()
+			#self.layer_normalization=TanhNormalization()
 			self.reg_normalization=MaxNormalization(mu=np.pi/2, l=0.5*(2/np.pi)**0.5, p=2)
 		self.scale = scale
 		self.seed=seed
@@ -157,7 +157,7 @@ class RandomFourierFeatures(tf.keras.layers.Layer):
 		outputs = tf.raw_ops.MatMul(a=inputs, b=kernel)
 		outputs = tf.nn.bias_add(outputs, self.bias)
 		
-		outputs=self.layer_normalization(outputs)
+		#outputs=self.layer_normalization(outputs)
 
 		outputs=self.reg_normalization(outputs)
 
