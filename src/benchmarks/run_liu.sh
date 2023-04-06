19d0fcb20aa06e297bfa48cc248472a744a6c0c6
DCalhas <daivdcalhas@hotmail.com>
2023-04-06 08:40:19 +0100

    run liu with less memory

--- src/benchmarks/run_liu.sh
+++ src/benchmarks/run_liu.sh
@@ -1,5 +1,5 @@
-python liu.py uncertainty $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 2
-python liu.py uncertainty $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 3
-python liu.py uncertainty $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 5
-python liu.py uncertainty $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 7
-python liu.py uncertainty $1 -variational -variationalDCT -interval_eeg 1 -save -memory_limit 6000 -seed 11
+python liu.py uncertainty $1 -interval_eeg 1 -save -memory_limit 3000 -seed 2
+python liu.py uncertainty $1 -interval_eeg 1 -save -memory_limit 3000 -seed 3
+python liu.py uncertainty $1 -interval_eeg 1 -save -memory_limit 3000 -seed 5
+python liu.py uncertainty $1 -interval_eeg 1 -save -memory_limit 3000 -seed 7
+python liu.py uncertainty $1 -interval_eeg 1 -save -memory_limit 3000 -seed 11
